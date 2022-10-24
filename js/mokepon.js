1f556f298263cd7a34be610286359a591d7af996
Ed soto <Edsghot@gmail.com>
2022-10-23 21:27:37 -0500

    agregando ataques de los enemigos y jugador

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,6 +1,25 @@
 
+/*
+:::::::VARIABLES GLOBALES::::::::*/
+
+let mascotas = ['hipodoge','capipepo','ratigueya','langostelvis','tucapalma','pydos']
+let ataqueJugador,ataqueEnemigo 
+//::::::::FIN:::::::::::::
+
+//funcion para generar numero aletorios en un rango
+function aleatorio(min,max){
+    return Math.floor(Math.random() * (max-min+1)+min)
+}
+
+//funcion para seleccionar el jugador del enemigo
+function seleccionarMascotaEnemigo(){
+    let ataqueAleatorio = aleatorio(0,5)
+    document.getElementById('spMascotaEnemigo').innerHTML = (mascotas[ataqueAleatorio].toUpperCase())
+}
+
+//funcion para selecionar la mascota del jugador
 function seleccionarMascota(){
-    let mascotas = ['hipodoge','capipepo','ratigueya','langostelvis','tucapalma','pydos']
+    seleccionarMascotaEnemigo()
     let c=0;
     for(let i=0;i<mascotas.length;i++){
         if(document.getElementById(mascotas[i]).checked){
@@ -12,12 +31,48 @@ function seleccionarMascota(){
     if(c==mascotas.length){
         alert("error!... Selecciona una mascota!!")
     }
+
+    //llamando la funcion para escoger la mascota del enemigo
+   
+}
+function ataqueAgua(){
+    ataqueJugador = 'Agua'
+    alert(ataqueJugador)
+}
+function ataqueFuego(){
+    ataqueJugador = 'Fuego'
+    alert(ataqueJugador)
+}
+function ataqueTierra(){
+    ataqueJugador = 'Tierra'
+    alert(ataqueJugador)
+}
+function ataqueEnemigo(){
+    let ataque = aleatorio(1,3)
+    if(ataque == 1){
+        ataqueEnemigo = 'Agua'
+    }else if(ataque ==2){
+        ataqueEnemigo = 'Fuego'
+    }else{
+        ataqueEnemigo = 'Tierra'
+    }
+    alert(ataqueEnemigo)
 }
 
+//funcion MAIN donde inicia todo el juego
 function iniciarJuego(){
     let btnMascotaJugador = document.getElementById('btnSeleccionar')
     btnMascotaJugador.addEventListener('click',seleccionarMascota)
 
+    
+    let btnFuego = document.getElementById('btnFuego')
+    btnFuego.addEventListener('click',ataqueFuego)
+    let btnAgua = document.getElementById('btnAgua')
+    btnAgua.addEventListener('click',ataqueAgua)
+    let btnTierra = document.getElementById('btnTierra')
+    btnTierra.addEventListener('click',ataqueTierra)
+
+    
 }
 
 //llamando a la funcion iniciar con el evento load(cuando cargue la pagina)
